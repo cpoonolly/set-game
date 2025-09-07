be525ba82a4c7d6c9d4e6212a15959d95ffe048f
Indi Rinearson <indi.rinearson@gmail.com>
2025-09-07 11:33:31 -0700

    clean up found sets

--- src/FoundSets.tsx
+++ src/FoundSets.tsx
@@ -7,50 +7,24 @@ interface FoundSetsProps {
 }
 
 export const FoundSets: FC<FoundSetsProps> = ({ sets, setCount }) => {
-  const [setOne, setTwo, setThree, setFour, setFive, setSix] = sets;
-
   return (
     <div className="flex flex-col gap-y-6.5">
       <h3 className="text-lg font-bold">Found Sets {setCount}</h3>
 
-      <table>
-        <tr className="h-16">
-          <td className="w-24">
-            {setOne && <Card card={setOne[0]} readOnly />}
-          </td>
-          <td className="w-24">
-            {setOne && <Card card={setOne[1]} readOnly />}
-          </td>
-          <td className="w-24">
-            {setOne && <Card card={setOne[2]} readOnly />}
-          </td>
-        </tr>
-        <tr className="h-16">
-          <td>{setTwo && <Card card={setTwo[0]} readOnly />}</td>
-          <td>{setTwo && <Card card={setTwo[1]} readOnly />}</td>
-          <td>{setTwo && <Card card={setTwo[2]} readOnly />}</td>
-        </tr>
-        <tr className="h-16">
-          <td>{setThree && <Card card={setThree[0]} readOnly />}</td>
-          <td>{setThree && <Card card={setThree[1]} readOnly />}</td>
-          <td>{setThree && <Card card={setThree[2]} readOnly />}</td>
-        </tr>
-        <tr className="h-16">
-          <td>{setFour && <Card card={setFour[0]} readOnly />}</td>
-          <td>{setFour && <Card card={setFour[1]} readOnly />}</td>
-          <td>{setFour && <Card card={setFour[2]} readOnly />}</td>
-        </tr>
-        <tr className="h-16">
-          <td>{setFive && <Card card={setFive[0]} readOnly />}</td>
-          <td>{setFive && <Card card={setFive[1]} readOnly />}</td>
-          <td>{setFive && <Card card={setFive[2]} readOnly />}</td>
-        </tr>
-        <tr className="h-16">
-          <td>{setSix && <Card card={setSix[0]} readOnly />}</td>
-          <td>{setSix && <Card card={setSix[1]} readOnly />}</td>
-          <td>{setSix && <Card card={setSix[2]} readOnly />}</td>
-        </tr>
-      </table>
+      <div className="flex flex-col">
+        {sets.length > 0 ? (
+          sets.map((set, index) => (
+            <div key={index} className="flex flex-row">
+              {set.map((card, index) => (
+                <Card key={index} card={card} readOnly />
+              ))}
+            </div>
+          ))
+        ) : (
+          // Empty space for the found sets to be displayed
+          <div className="w-76" />
+        )}
+      </div>
     </div>
   );
 };
