f25e10c444c15990a0af57d1b14e5dbf4b5378ad
Indi Rinearson <indi.rinearson@gmail.com>
2025-09-12 21:17:48 -0700

    fix last event display for small screens

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import Card from "./Card";
 import { SetGame } from "./SetGame";
-import { Card as CardType } from "./types";
+import { Card as CardType, GameEvent } from "./types";
+import { SET_SIZE } from "./constants";
 
 interface GameBoardProps {
   formattedTime: string;
   game: SetGame;
   selectedCards: Immutable.Set<CardType>;
   handleCardClick: (card: CardType) => void;
+  lastEvent: GameEvent;
 }
 
 export const GameBoard: FC<GameBoardProps> = ({
@@ -15,6 +17,7 @@ export const GameBoard: FC<GameBoardProps> = ({
   game,
   selectedCards,
   handleCardClick,
+  lastEvent,
 }) => {
   return (
     <div className="flex flex-col gap-y-7">
@@ -22,6 +25,7 @@ export const GameBoard: FC<GameBoardProps> = ({
         <p>Time: {formattedTime}</p>
         {game.isComplete && <p className="text-green-600">Game Complete! 🎉</p>}
       </div>
+
       <div className="grid grid-cols-3 xl:grid-cols-4 gap-2.5 max-w-4xl justify-center items-center">
         {Array.from(game.board.cards).map((card) => (
           <Card
@@ -32,6 +36,23 @@ export const GameBoard: FC<GameBoardProps> = ({
           />
         ))}
       </div>
+
+      {lastEvent && selectedCards.size === SET_SIZE ? (
+        <div className="font-bold text-lg">
+          {lastEvent.type === 0 && (
+            <p className="text-green-600">Set found! ✓</p>
+          )}
+          {lastEvent.type === 1 && (
+            <p className="text-amber-500">Set already found!</p>
+          )}
+          {lastEvent.type === 2 && (
+            <p className="text-red-600">Invalid set! ✗</p>
+          )}
+        </div>
+      ) : (
+        // Empty space for the last event to be displayed
+        <div className="h-7" />
+      )}
     </div>
   );
 };
