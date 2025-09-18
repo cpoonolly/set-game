8cd0362f37e4878560300a848b9e02f16014932f
Indi Rinearson <indi.rinearson@gmail.com>
2025-09-18 10:52:20 -0700

    convert rectangles to squiggles

--- src/types.ts
+++ src/types.ts
@@ -1,50 +1,50 @@
-import { Set } from 'immutable';
+import { Set } from "immutable";
 
 export type Card = string;
 
 export enum Shape {
-    RECTANGLE = 'a',
-    OVAL = 'b', 
-    DIAMOND = 'c'
+  SQUIGGLE = "a",
+  OVAL = "b",
+  DIAMOND = "c",
 }
 
 export enum Color {
-    GREEN = 'a',
-    RED = 'b',
-    PURPLE = 'c'
+  GREEN = "a",
+  RED = "b",
+  PURPLE = "c",
 }
 
 export enum Fill {
-    SOLID = 'a',
-    EMPTY = 'b',
-    STRIPED = 'c'
+  SOLID = "a",
+  EMPTY = "b",
+  STRIPED = "c",
 }
 
 export enum Count {
-    ONE = 'a',
-    TWO = 'b',
-    THREE = 'c'
+  ONE = "a",
+  TWO = "b",
+  THREE = "c",
 }
 
 export interface CardProperties {
-    shape: Shape;
-    color: Color;
-    fill: Fill;
-    count: Count;
+  shape: Shape;
+  color: Color;
+  fill: Fill;
+  count: Count;
 }
 
 export interface Board {
-    cards: Set<Card>,
-    sets: Set<Set<Card>>
+  cards: Set<Card>;
+  sets: Set<Set<Card>>;
 }
 
 export enum GameEventType {
-    SET_FOUND,
-    SET_ALREADY_FOUND,
-    INVALID_SET,
+  SET_FOUND,
+  SET_ALREADY_FOUND,
+  INVALID_SET,
 }
 
 export interface GameEvent {
-    type: GameEventType;
-    set?: Set<Card>;
+  type: GameEventType;
+  set?: Set<Card>;
 }
